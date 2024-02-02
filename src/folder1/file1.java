a8657108bca22eb7726ea440e9dcff38a2f7fd6e
SiddharthMurjani <110025928+SiddharthMurjani@users.noreply.github.com>
2024-02-02 15:33:39 +0530

    Update file1.java

--- src/folder1/file1.java
+++ src/folder1/file1.java
@@ -3,11 +3,11 @@ package folder1;
 import folder2.File2;
 
 public class File1 {
-    public static void file1Method() {
+    public static void main() {
         System.out.println("Inside file1Method");
     }
 
-    public static void main(String[] args) {
+    public static void file1Method(String[] args) {
         file1Method();
         File2.file2Method();
     }
